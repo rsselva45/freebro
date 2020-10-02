1138e503f05214e57fc48e955572ad665ca02732
rsselva45 <rselva216@gmail.com>
2020-10-02 09:28:24 +0530

    my page.

---
+++ mod4-asoln/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+
+	//3//
+
+(function(window){
+	var byeSpeaker={};
+	var speakWord = "Good Bye";
+	byeSpeaker.speak = function(Name) {
+  	console.log(speakWord+Name);
+	}
+	window.byeSpeaker=byeSpeaker;
+})(window);
---
+++ mod4-asoln/SpeakHello.js
@@ -0,0 +1,11 @@
+
+	//2//
+
+(function(window){
+	var helloSpeaker={};
+	var speakWord = "Hello";
+	helloSpeaker.speak = function(Name) {
+  	console.log(speakWord+Name);
+	}
+	window.helloSpeaker=helloSpeaker;
+})(window);
