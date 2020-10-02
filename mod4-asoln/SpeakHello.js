8cb9586f05aae61558aa8ebca1316088960018df
rsselva45 <rselva216@gmail.com>
2020-10-02 09:51:41 +0530

    my page.

--- mod4-asoln/SpeakHello.js
+++ mod4-asoln/SpeakHello.js
@@ -5,7 +5,7 @@
 	var helloSpeaker={};
 	var speakWord = "Hello";
 	helloSpeaker.speak = function(Name) {
-  	console.log(speakWord+Name);
+  	console.log(speakWord,Name);
 	}
 	window.helloSpeaker=helloSpeaker;
 })(window);
